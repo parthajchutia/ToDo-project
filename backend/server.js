f197075561fbee2e99a749e112a828655496e659
Partha_jyoti_chutia <119157042+parthajchutia@users.noreply.github.com>
2024-09-27 00:47:58 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require(`express`);
+const mongoose = require(`mongoose`);
+const routes = require('./routes/ToDoRoute');
+const cors = require('cors');
+
+
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json())
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URL).then(()=> console.log(`Connected To MongoDB...`)).catch((err) => console.log(err))
+
+app.use(routes);
+
+app.listen(PORT , ()=>console.log(`listening the port on: ${PORT}`))
