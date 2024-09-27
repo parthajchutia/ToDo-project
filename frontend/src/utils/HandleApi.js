99db38fcc717f4f62b0a18c6ee841a90c597e47e
Partha_jyoti_chutia <119157042+parthajchutia@users.noreply.github.com>
2024-09-27 18:40:26 +0530

    deploy

--- frontend/src/utils/HandleApi.js
+++ frontend/src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5001";
+const baseUrl = "https://fullstack-to-do-backend-hqnc.onrender.com";
 
 const getAllToDo = (setToDo) => {
   axios.get(baseUrl).then(({ data }) => {
