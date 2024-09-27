edd28dec2a771fa9642691ff4410453e0a7e5396
Partha_jyoti_chutia <119157042+parthajchutia@users.noreply.github.com>
2024-09-27 18:59:11 +0530

    Update App.js
    Signed-off-by: Partha_jyoti_chutia <119157042+parthajchutia@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
     if (toDo.length === 0) {
       const timer = setTimeout(() => {
         window.location.reload();
-      }, 2000);
+      }, 6000);
 
       return () => clearTimeout(timer);
     }
